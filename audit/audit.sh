42b4b19b267c8b15f222cf381fbf85447ad9006f
adi7312 <114097246+adi7312@users.noreply.github.com>
2024-05-20 21:16:29 +0200

    Audit script fix

--- audit/audit.sh
+++ audit/audit.sh
@@ -9,14 +9,14 @@ else
 fi
 echo "[*] Performing an audit of the image ghcr.io/adi7312/vuln-scan:latest..."
 sudo docker scout cves --format only-packages --output vuln_scan_image_audit.txt --exit-code ghcr.io/adi7312/vuln-scan:latest 
-if [ $? -ne 0 ]; then
+if [ $? -ne 2 ]; then
     echo "[+] No vulnerabilities found. Audit completed with 0 vulnerabilities"
     exit 0
 fi
 echo "[!] Found vulnerabilities. Audit completed. Results saved to vuln_scan_image_audit.txt."
 echo "[*] Performing propability analysis of exploiting vulnerabilities..."
 sudo docker scout cves --epss --epss-score 0.2 --output vuln_scan_prob_analysis.txt --exit-code ghcr.io/adi7312/vuln-scan:latest
-if [ $? -ne 0 ]; then
+if [ $? -ne 2 ]; then
     echo "[+] Probability of found vulnerabilities being exploited is low. Anaylsis completed."
     exit 0
 fi
