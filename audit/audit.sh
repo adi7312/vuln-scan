53823e241a36968e78fc5df3b595f8babd13ec9a
adi7312 <114097246+adi7312@users.noreply.github.com>
2024-05-26 16:33:06 +0200

    script update

--- audit/audit.sh
+++ audit/audit.sh
@@ -1,26 +1,26 @@
-echo "[!] WARNING: For this operation you need to be authentictaed into DockerHub!"
+echo -e "\e[0;31m[!]\e[m WARNING: For this operation you need to be authentictaed into DockerHub!"
 if ! docker scout version &> /dev/null; then
-    echo "[!] Docker Scout is not installed."
-    echo "[*] Installing Docker Scout..."
+    echo -e "\e[0;31m[!]\e[m Docker Scout is not installed."
+    echo -e "\e[0;36m[*]\e[m Installing Docker Scout..."
     curl -fsSL https://raw.githubusercontent.com/docker/scout-cli/main/install.sh -o install-scout.sh
     sudo sh install-scout.sh
-    echo "[+] Docker Scout has been installed successfully."
+    echo -e "\e[0;32m[+]\e[m Docker Scout has been installed successfully."
 else
-    echo "[*] Docker Scout is installed."
+    echo -e "\e[0;36m[*]\e[m Docker Scout is installed."
 fi
-echo "[*] Performing an audit of the image ghcr.io/adi7312/vuln-scan:latest..."
+echo -e "\e[0;36m[*]\e[m Performing an audit of the image ghcr.io/adi7312/vuln-scan:latest..."
 sudo docker scout cves --format only-packages --output vuln_scan_image_audit.txt --exit-code ghcr.io/adi7312/vuln-scan:latest 
 if [ $? -ne 2 ]; then
-    echo "[+] No vulnerabilities found. Audit completed with 0 vulnerabilities"
+    echo -e "\e[0;32m[+]\e[m No vulnerabilities found. Audit completed with 0 vulnerabilities"
     exit 0
 fi
-echo "[!] Found vulnerabilities. Audit completed. Results saved to vuln_scan_image_audit.txt."
-echo "[*] Performing propability analysis of exploiting vulnerabilities..."
+echo -e "\e[0;31m[!]\e[m Found vulnerabilities. Audit completed. Results saved to vuln_scan_image_audit.txt."
+echo -e "\e[0;36m[*]\e[m Performing propability analysis of exploiting vulnerabilities..."
 sudo docker scout cves --epss --epss-score 0.2 --output vuln_scan_prob_analysis.txt --exit-code ghcr.io/adi7312/vuln-scan:latest
 if [ $? -ne 2 ]; then
-    echo "[+] Probability of found vulnerabilities being exploited is low. Anaylsis completed."
+    echo -e "\e[0;32m[+]\e[m Probability of found vulnerabilities being exploited is low. Anaylsis completed."
     exit 0
 fi
-echo "[!] Probability of found vulnerabilities being exploited is above 20%. Anaylsis completed. Results saved to vuln_scan_prob_analysis.txt."
-echo "[*] Generating SBOM for the image ghcr.io/adi7312/vuln-scan:latest..."
+echo -e "\e[0;31m[!]\e[m Probability of found vulnerabilities being exploited is above 20%. Anaylsis completed. Results saved to vuln_scan_prob_analysis.txt."
+echo -e "\e[0;36m[*]\e[m Generating SBOM for the image ghcr.io/adi7312/vuln-scan:latest..."
 sudo docker sbom ghcr.io/adi7312/vuln-scan:latest --output vuln_scan_image_sbom.txt
--- start.sh
+++ start.sh
@@ -2,61 +2,61 @@
 
 
 if ! command -v docker &> /dev/null; then
-    echo "[!] Docker is not installed."
-    echo "[!] Warning: Script supports installing Docker on Ubuntu and Debian systems only!"
-    echo "[*] Updating package lists..."
+    echo -e "\e[0;31m[!]\e[m Docker is not installed."
+    echo -e "\e[0;31m[!]\e[m Warning: Script supports installing Docker on Ubuntu and Debian systems only!"
+    echo -e "\e[0;36m[*]\e[m Updating package lists..."
     sudo apt-get update -y
-    echo "[*] Installing Docker..."
+    echo -e "\e[0;36m[*]\e[m Installing Docker..."
     sudo apt install docker.io -y
-    echo "[+] Docker has been installed and started successfully."
+    echo -e "\e[0;31m\e[0;32m[*]\e[m\e[m Docker has been installed and started successfully."
 else
-    echo "[*] Docker is installed."
+    echo -e "\e[0;36m[*]\e[m Docker is installed."
 fi
 
 sudo service docker start
 
 if [ -z "$FREQUENCY" ]; then
-    echo "[!] FREQUENCY is not set."
-    echo "[!] Please set the FREQUENCY environment variable."
+    echo -e "\e[0;31m[!]\e[m FREQUENCY is not set."
+    echo -e "\e[0;31m[!]\e[m Please set the FREQUENCY environment variable."
     exit 1
 fi
 
 if [ -z "$EMAIL" ]; then
-    echo "[!] EMAIL is not set."
-    echo "[!] Please set the EMAIL environment variable."
+    echo -e "\e[0;31m[!]\e[m EMAIL is not set."
+    echo -e "\e[0;31m[!]\e[m Please set the EMAIL environment variable."
     exit 1
 fi
 
 if [ -z "$IP" ]; then
-    echo "[!] IP is not set."
-    echo "[!] Please set the IP environment variable."
+    echo -e "\e[0;31m[!]\e[m IP is not set."
+    echo -e "\e[0;31m[!]\e[m Please set the IP environment variable."
     exit 1
 fi
 
 if [ -z "$SENDER_PASS" ]; then
-    echo "[!] SENDER_PASS is not set."
-    echo "[!] Please set the SENDER_PASS environment variable."
+    echo -e "\e[0;31m[!]\e[m SENDER_PASS is not set."
+    echo -e "\e[0;31m[!]\e[m Please set the SENDER_PASS environment variable."
     exit 1
 fi
 
 if [ -z "$USERNAME" ]; then
-    echo "[!] USERNAME is not set. Switching to default username: admin."
+    echo -e "\e[0;31m[!]\e[m USERNAME is not set. Switching to default username: admin."
     USERNAME="admin"
 fi
 
 if [ -z "$PASSWORD" ]; then
-    echo "[!] PASSWORD is not set. Switching to default password: admin."
+    echo -e "\e[0;31m[!]\e[m PASSWORD is not set. Switching to default password: admin."
     PASSWORD="admin"
 fi
 
-echo "[*] Pulling ghcr.io/adi7312/vuln-scan:latest..."
+echo -e "\e[0;36m[*]\e[m Pulling ghcr.io/adi7312/vuln-scan:latest..."
 sudo docker pull ghcr.io/adi7312/vuln-scan:latest
 # if --audit flag is passed perform below operation
 if [ "$1" = "--audit-enable" ]; then
-    echo "[*] Running the audit script..."
+    echo -e "\e[0;36m[*]\e[m Running the audit script..."
     sudo bash audit/audit.sh
 fi
-echo "[*] Running the container in detached mode..."
+echo -e "\e[0;36m[*]\e[m Running the container in detached mode..."
 sudo docker run --detach --publish 8090:9392 -e SKIPSYNC=true -e IP=$IP -e FREQUENCY=$FREQUENCY -e SENDER_PASS="$SENDER_PASS" -e EMAIL=$EMAIL -e USERNAME=$USERNAME -e PASSWORD=$PASSWORD --name avs ghcr.io/adi7312/vuln-scan:latest
 sudo docker exec avs /bin/bash /opt/app/config/setup_cron.sh
 sudo docker exec -it avs python3 /opt/app/gvm_handler.py
